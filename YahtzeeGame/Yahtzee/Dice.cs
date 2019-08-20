c65e861d88cf890f4fb15403a49e1bc8deffd100
Hector Gonzalez <41347749+gonzalezsoriano777@users.noreply.github.com>
2019-08-20 18:40:31 -0500

    Update Dice.cs
    testing...

--- YahtzeeGame/Yahtzee/Dice.cs
+++ YahtzeeGame/Yahtzee/Dice.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,43 +6,22 @@ using System.Threading.Tasks;
 
 namespace YahtzeeGame
 {
-    public class Dice
+    class Program
     {
-        Random dice = new Random();
-        int[] gameDice = new int[5];
-
-        public void rollDice()
+        static void Main(string[] args)
         {
-            for(int diceRoll = 0; diceRoll < gameDice.Length; diceRoll++)
+
+            Dice newDice = new Dice();
+
+            int[][] diceResult = newDice.rollDicesThreeTimes();
+
+            foreach (int[] row in diceResult)
             {
-                var random = dice.Next(1, 7);
-                gameDice[diceRoll] = random;
-                Console.WriteLine(gameDice[diceRoll]);
-            }
-        }
-
-        internal int[][] rollDicesThreeTimes()
-        {
-            throw new NotImplementedException("No argument has been implemented.");
-        }
-
-        public int[,] rollDicesThreeTimes(int numOfRolls, int numOfDices)
-        {
-            int[,] result = new int[numOfRolls, numOfDices];
-
-            int rollCount = 0;
-
-            while(rollCount < numOfRolls)
-            {
-                for(int i = 0; i < numOfDices; i++)
+                foreach (var dice in row)
                 {
-                    result[rollCount, i] = dice.Next(1, 7);
+                    Console.WriteLine(dice);
                 }
-
-                rollCount++;
             }
-
-            return result;
         }
     }
 }
